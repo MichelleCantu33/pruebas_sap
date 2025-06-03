9d62e3828fa606407b95feee28390a6cb61b6a73
MichelleCantu26 <michellealex738@gmail.com>
2025-06-03 15:20:12 -0500

    pruebas nuevo modulo cajas instrumental 1

--- routes.py
+++ routes.py
@@ -712,20 +712,34 @@ def crear_caja_instrumental():
     try:
         cursor = conn.cursor()
 
+        # Obtener el último DocEntry
+        cursor.execute('SELECT MAX("DocEntry") FROM "EC_SBO_BIOCELLS_PROD"."@LS_CAJ_CAB"')
+        ultimo_docentry = cursor.fetchone()[0] or 0
+        nuevo_docentry = ultimo_docentry + 1
+
         # Insertar cabecera
         cursor.execute("""
-            INSERT INTO "PRU_BIOCELLS_20250509"."@LS_CAJ_CAB" ("Code", "U_LS_FECHA", "U_LS_ALM", "U_LS_CLASECAJA")
-            VALUES (?, ?, ?, ?)
-        """, (data["CodigoCaja"], data["FechaCaja"], data["Almacen"], data["ClaseCaja"]))
+            INSERT INTO "EC_SBO_BIOCELLS_PROD"."@LS_CAJ_CAB" 
+            ("DocEntry", "Code", "U_LS_FECHA", "U_LS_ALM", "U_LS_CLASECAJA")
+            VALUES (?, ?, ?, ?, ?)
+        """, (
+            nuevo_docentry,
+            data["CodigoCaja"],
+            data["FechaCaja"],
+            data["Almacen"],
+            data["ClaseCaja"]
+        ))
 
         # Insertar líneas
-        for linea in data["Lineas"]:
+        for i, linea in enumerate(data["Lineas"], start=1):
             cursor.execute("""
-                INSERT INTO "PRU_BIOCELLS_20250509"."@LS_CAJ_LIN" ("Code", "LineId", "U_LS_ITEM", "U_LS_CANT", "U_LS_TIPO", "U_LS_LOTE")
-                VALUES (?, ?, ?, ?, ?, ?)
+                INSERT INTO "EC_SBO_BIOCELLS_PROD"."@LS_CAJ_LIN"
+                ("DocEntry", "Code", "LineId", "U_LS_ITEM", "U_LS_CANT", "U_LS_TIPO", "U_LS_LOTE")
+                VALUES (?, ?, ?, ?, ?, ?, ?)
             """, (
+                nuevo_docentry,
                 data["CodigoCaja"],
-                linea["NumeroLinea"],
+                i,
                 linea["CodigoItem"],
                 linea["CantidadItem"],
                 linea["TipoItem"],
@@ -733,13 +747,14 @@ def crear_caja_instrumental():
             ))
 
         conn.commit()
-        return jsonify({"mensaje": "Caja creada correctamente"}), 201
+        return jsonify({"mensaje": "Caja creada correctamente", "DocEntry": nuevo_docentry}), 201
 
     except Exception as e:
         conn.rollback()
         return jsonify({"error": str(e)}), 500
     finally:
         conn.close()
+
         
 @app.route('/cajas-instrumental', methods=['GET'])
 def obtener_cajas_instrumental():
